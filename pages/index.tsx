31c7ba5c170ea2e14a2e49d1218895fed7e5ac08
Abheek Dhawan <67982792+abheekda1@users.noreply.github.com>
2023-08-27 14:42:48 -0500

    fix: temporarily disable form to email

--- pages/index.tsx
+++ pages/index.tsx
@@ -322,6 +322,7 @@ const About: NextPage = () => {
                             type='submit'
                             value='Submit'
                             className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                            disabled
                         />
                     </form>
                     <p className='block w-full text-center text-slate-700 dark:text-slate-50 mt-2'>
